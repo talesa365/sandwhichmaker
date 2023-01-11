e9fb0ddc8a5a2be183c1ffbb3c8e47259bcbd76e
talesa365 <talesahowell7@gmail.com>
2023-01-11 18:09:55 -0500

    new repo created new project

---
+++ sandwhichmaker/sandwhichapp/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path
+from sandwichapp.views import SandwichappView, IngredientsListView, SandwichGeneratorView
+
+
+urlpatterns=[
+    path('', SandwichappView.as_view(), name='sandwich'),
+    path('ingredients/<str:ingredient_type>', IngredientsListView.as_view(), name='ingredients_list'),
+    path('random', SandwichGeneratorView.as_view(), name='sandwich_generator'),
+]
